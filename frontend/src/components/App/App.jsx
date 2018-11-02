f63158d19e3b70880503f75887e50e71c069a09b
sun1211 <ngoctam121191@yahoo.com>
2018-11-02 10:38:31 +0700

    add demo login GUI to front end

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -1,16 +1,24 @@
 // React core
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+// Components
+import { Login } from 'components';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-      Dice Game
+        <Login />
       </div>
     );
   }
 
 }
 
-export default App;
+// Map all state to component props (for redux to connect)
+const mapStateToProps = state => state;
+
+// Export a redux connected component
+export default connect(mapStateToProps)(App);
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,5 +1,7 @@
 import App from './App';
+import Login from './Login';
 
 export {
   App,
+  Login,
 }
