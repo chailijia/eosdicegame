c250d42fb77c58a37452f1d18870911d6fdaefb2
quoc le <quocle@orgos.net>
2018-12-09 21:31:24 +0700

    feature: dice table

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -2,17 +2,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // Components
-import { Login, Card, GameInfo, PlayerInfo, GameMat, Game, Header,BetTable } from 'components';
+import { Login, Card, GameInfo, PlayerInfo, GameMat, Game, Header, BetTable } from 'components';
+import DiceBoard from 'components/DiceBoard/DiceBoard.jsx';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-      <Header/>  
-      <BetTable/>    
-      <Game/>
-      
+        <Header />
+          <div className="dice_board">
+            <DiceBoard />
+          </div>
+          <div className="dice_table">
+            <BetTable />
+          </div>
       </div>
     );
   }
